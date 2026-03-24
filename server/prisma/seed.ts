08584753fd1905896b46957c68733d2733d4ba8f
ngothtsika <ngothtsikavivien@gmail.com>
2026-03-24 16:36:28 +0000

    update seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,12 @@
-import prisma from "../src/utils/prisma";
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool as any);
+const prisma = new PrismaClient({ adapter });
 
 /**
  * Seed script to populate initial data
